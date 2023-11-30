b9845a39b9e74fe64f75dd34e358125f7bbd9fa4
AndyTue <andy_djte@hotmail.com>
2023-11-29 22:35:06 -0600

    emir pasa bien el codigo para la siguiente porfa

---
+++ programa.php
@@ -0,0 +1,36 @@
+<!DOCTYPE htmL>
+<html lang='es'>
+    <head>
+        <meta charset='utf-8' />
+        <link rel="stylesheet" href="estilo.css" type="text/CSS">
+        <title>Estructura básica de una página web en HTMLS</title>
+  
+
+    </head>
+    <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href ='index.html'>Inicio</a></li>
+                <li><a href ='index2.html'>Quién soy</a></li>
+                <li><a href ='galeria.html'>Galería </a></li>
+                <li><a href ='contacto.html'>Contacto</a></li>
+                <li id="amasound"><a href="carrito.php">AmaSounds</a></li>
+                <li><a href ='login.html'>Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <aside>
+        <p>Bienvenido</p>
+    </aside>
+    <header>
+        <nav>
+            <ul>
+                <li><a href='listaUsuarios.php'>Lista usuarios</a></li>
+                <li><a href='registro.html'>Registra usuario</a></li>
+
+            </ul>
+        </nav>
+    </header>
+    </body>
+</html>
